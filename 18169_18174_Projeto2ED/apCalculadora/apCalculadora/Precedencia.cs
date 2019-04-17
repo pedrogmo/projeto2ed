5e5051093a2117c0f417cd7bef116703c1b8db71
Samuel Dias <42343502+Nikenilson@users.noreply.github.com>
2019-04-17 14:35:59 -0300

    aa

--- 18169_18174_Projeto2ED/apCalculadora/apCalculadora/Precedencia.cs
+++ 18169_18174_Projeto2ED/apCalculadora/apCalculadora/Precedencia.cs
@@ -6,36 +6,23 @@ using System.Threading.Tasks;
 
 namespace apCalculadora
 {
-    static class Precedencia
+    class Precedencia
     {
-        private static bool[,] matriz;
-        private int qtdLinhas;
+        private bool[,] matriz = new bool[7, 7];
 
-        public static bool HaPrecedencia(char topo, char lido)
+        public Precedencia()
+        {
+            matriz[0, 0] = matriz[0, 1] = matriz[0, 2] = matriz[0, 3] = matriz[0, 4] = matriz[0, 5] = 
+            matriz[0, 1] = matriz[2, 0] = matriz[2, 1] = matriz[3,0] = matriz[3, 1] = matriz[4,0] = 
+            matriz[4,1] = matriz[4,2] = matriz[4,3] = matriz[5,0] = matriz[5,1] = matriz[5,2] = matriz[5,3] =
+            matriz[6,0] = matriz[6,1] = matriz[6,2] = matriz[6,3] = matriz[6,4] = matriz[6,5] = matriz[6,6] = false;
+            matriz[0,6] = matriz[1,1] = matriz[1,2] = matriz[1,3] = matriz[1,4] = matriz[1,5] = matriz[1,6] = 
+            matriz= true
+        }
+        public bool HaPrecedencia(char topo, char lido)
         {
             
         }
-        private int GetChar(char c)
-        {
-
-            for (int j = 0; j < matriz.GetLowerBound(2); j++)
-            {
-                if (days[j] == testDay)
-                {
-                    return j;
-                }
-            }
-
-            throw new System.ArgumentOutOfRangeException(testDay, "testDay must be in the form \"Sun\", \"Mon\", etc");
-        }
-
-        // The get accessor returns an integer for a given string
-        public int this[string day]
-        {
-            get
-            {
-                return (GetDay(day));
-            }
-        }
+        
     }
 }
