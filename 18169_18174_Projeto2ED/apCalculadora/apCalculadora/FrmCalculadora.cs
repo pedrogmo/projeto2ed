d9d43ccccfa91ec230a1296416083a66e1d2bea9
pedrogmo <42345539+pedrogmo@users.noreply.github.com>
2019-05-08 08:34:49 -0300

    Comentários da classe form e precedência

--- 18169_18174_Projeto2ED/apCalculadora/apCalculadora/FrmCalculadora.cs
+++ 18169_18174_Projeto2ED/apCalculadora/apCalculadora/FrmCalculadora.cs
@@ -15,19 +15,21 @@ namespace apCalculadora
 {
     public partial class FrmCalculadora : Form
     {
-        //objeto de sequência que será usado para calcular
+        //objeto de sequência que será usado para calcular:
         Sequencia sequencia;
-        //boolean de controle de habilitação da edição do textbox
+
+        //boolean de controle de habilitação da edição do textbox:
         bool edicaoPermitida;
 
         public FrmCalculadora()
         {
-            InitializeComponent();
-            edicaoPermitida = true;
+            InitializeComponent();            
+            edicaoPermitida = true; //edição inicialmente é permitida
         }
 
         private void FrmCalculadora_Load(object sender, EventArgs e)
         {
+            //no evento Load do formulário, é exibida uma mensagem de instrução para o usuário
             MessageBox.Show("Digite sua sequência pelo teclado ou clique nos botões. Para calculá-la, clique no"+
                 " botão '=' ou aperte a tecla '=' no teclado. Para criar outra sequência, você deve limpar a sequência" +
                 " anterior. Atenção: NÃO inclua parênteses em volta de números negativos, isso não é preciso!"
@@ -36,17 +38,22 @@ namespace apCalculadora
 
         private void Tudo_Click(object sender, EventArgs e)
         {
+            //se a edição está habilitada, adiciona-se no textbox o caractere do 
+            //botão pressionado
             if (edicaoPermitida)
                 AdicionarNoTxt(char.Parse((sender as Button).Text));
         }
 
         private void AdicionarNoTxt(char c)
         {
+            //texto do txtVisor concatena caractere passado como parâmetro
             txtVisor.Text += c;
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
+            //txtVisor, txtResultado e lblSequencias têm seus textos redefinidos
+            //para o valor original
             txtVisor.Text = "";
             txtResultado.Text = "";
             lblSequencias.Text = "Pósfixa:";
@@ -57,44 +64,58 @@ namespace apCalculadora
         {
             try
             {
-                if (edicaoPermitida)
+                if (edicaoPermitida) //se a edição está permitida, ou seja, se é uma nova sequência 
+                //digitada
                 {
-                    sequencia = new Sequencia(txtVisor.Text);
-                    string posfixo = sequencia.Posfixo;
-                    lblSequencias.Text = "Pósfixa:" + posfixo;
+                    sequencia = new Sequencia(txtVisor.Text); //instanciação de uma sequência com o texto
+                    //digitado no visor
+                    lblSequencias.Text = "Pósfixa:" + sequencia.Posfixo;
+                    //texto do lblSequências recebe valor da propriedade Posfixo da classe Sequencia
                     txtResultado.Text = sequencia.Resultado.ToString();
-                    edicaoPermitida = false;
+                    //texto do textbox resultado recebe o valor real Resultado convertido para string
+                    edicaoPermitida = false; //edição é desabilitada
+                    //o usuário só pode editar novamente quando a sequência é limpada
                 }
             }
             catch
             {
+                //caso haja algum erro, a programa exibe um alerta que indica que a sequência
+                //foi digitada incorretamente
                 MessageBox.Show("Sequência inválida", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void FrmCalculadora_KeyPress(object sender, KeyPressEventArgs e)
         {
-            char pressionado = (char)e.KeyChar;
-            if (edicaoPermitida)
+            //evento disparado quando usuário pressiona qualquer tecla do teclado
+            char pressionado = (char)e.KeyChar; //variável pressionado recebe a
+            //tecla do parâmetro e KeyPressEventArgs convertido para char
+            if (edicaoPermitida) //se a edição está habilitada, pode-se incluir o caractere no txtVisor
             {
                 if (pressionado == '0' || pressionado == '1' || pressionado == '2' || pressionado == '3' || pressionado == '4'
                      || pressionado == '5' || pressionado == '6' || pressionado == '7' || pressionado == '8' || pressionado == '9'
                       || pressionado == '+' || pressionado == '-' || pressionado == '*' || pressionado == '/' || pressionado == '^'
                       || pressionado == ',' || pressionado == '(' || pressionado == ')')
+                //se o char pressionado é algum número ou operador, ele é adicionado
                     AdicionarNoTxt(pressionado);                
             }
             if (pressionado == 'c' || pressionado == (char)Keys.Escape)
+            //se foi pressionada a tecla 'c' ou a tecla 'esc', dispara-se o evento click do btnClear
                 btnClear.PerformClick();
-            else if (pressionado == '=')
+            else if (pressionado == '=') //se foi pressionado '=', é disparado o click do btnIgual
                 btnIgual.PerformClick();
             else if (pressionado == (char)Keys.Back)
+            //por último, se foi pressionado o backspace, o btnBackspace tem seu método chamado
                 btnBackspace.PerformClick();
         }
 
         private void btnBackspace_Click(object sender, EventArgs e)
         {
             if (edicaoPermitida && txtVisor.Text != "")
+            //se a edição está habilitada e o valor do textbox não é uma cadeia vazia
                 txtVisor.Text = txtVisor.Text.Substring(0,txtVisor.TextLength - 1);
+                //valor do textbox recebe uma Substring que vai de seu início até o penúltimo índice
+                //dessa forma, o último caractere é removido
         }        
     }
  
--- 18169_18174_Projeto2ED/apCalculadora/apCalculadora/Precedencia.cs
+++ 18169_18174_Projeto2ED/apCalculadora/apCalculadora/Precedencia.cs
@@ -11,28 +11,40 @@ namespace apCalculadora
 {
     class Precedencia
     {
-        private bool[,] matriz;
+        private bool[,] matriz; //matriz de boolean que será usada para
+        //verificar a precedência do topo de uma pilha (linha) em relação
+        //a um operador lido (coluna)
+
         private static string texto =   "FFFFFFT" +
                                         "FFTTTTT" +
                                         "FFTTTTT" +
                                         "FFTTTTT" +
                                         "FFFFTTT" +
                                         "FFFFTTT";
+        //essa string foi copiada da apostila, com uma única alteração:
+        //o elevado topo não tem precedência sobre o elevado lido
+        //Exemplo: 2^4^2 = 2^16
 
         public Precedencia()
         {
-            matriz = new bool[6, 7];
+            matriz = new bool[6, 7]; //a matriz tem 6 linhas e 7 colunas
+            //os operadores são (, ^, *, /, +, - e )
+            //como o fecha parênteses nunca será empilhado, a sétima linha não é necessária
             int cont = 0;
-            for (int l = 0; l < 6; l++)
-                for (int c = 0; c < 7; c++)
+            for (int l = 0; l < 6; l++) //for de linha
+                for (int c = 0; c < 7; c++) //for de coluna
                     matriz[l, c] = texto[cont++] == 'T';
+                    //se o caractere do índice cont da string for igual a 'T', é armazenado true
+                    //se não for 'T', é armazenado false
+                    //incremento do cont
         }
 
         public bool HaPrecedencia(char topo, char lido)
         {
-            int linha = -1, coluna = -1;
+            int linha = -1, coluna = -1; //índices de linha e coluna recebem -1 inicialmente
             switch (topo)
             {
+                //linha recebe o índice respectivo do caractere topo, que vai de 0 a 5
                 case '(': linha = 0; break;
                 case '^': linha = 1; break;
                 case '*': linha = 2; break;
@@ -40,10 +52,12 @@ namespace apCalculadora
                 case '+': linha = 4; break;
                 case '-': linha = 5; break;
             }
-            if (linha == -1)
+            if (linha == -1) //se a linha permaneceu com o valor -1, significa que seu valor
+            //não foi trocado no switch e, portanto, o caractere é inválido
                 throw new Exception("Caractere de topo inválido");
             switch (lido)
             {
+                //coluna recebe o índice do operador, que vai de 0 a 6
                 case '(': coluna = 0; break;
                 case '^': coluna = 1; break;
                 case '*': coluna = 2; break;
@@ -52,9 +66,10 @@ namespace apCalculadora
                 case '-': coluna = 5; break;
                 case ')': coluna = 6; break;
             }
-            if (coluna == -1)
+            if (coluna == -1) //se continuar -1, char lido não é válido
                 throw new Exception("Caractere lido inválido");
-            return matriz[linha, coluna];
+
+            return matriz[linha, coluna]; //retorna-se a matriz de bool indexada de linha e coluna
         }        
     }
 }
